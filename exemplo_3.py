0163452b4c58c4932a0c7733b9dd3d3a7745c930
Mari Hammel <hammel2006@gmail.com>
2025-07-28 16:58:08 -0300

    Add files via upload

---
+++ exemplo_3.py
@@ -0,0 +1,23 @@
+#Solicitar os dados para o usuário
+v1 = int(input("\n\nInforme o primeiro número inteiro: "))
+v2 = int(input("Informe o segundo número inteiro: "))
+print("\n")
+#Exibindo o resultado das operações matemáticas
+print("•–––––-☆Sistema Para Calcular as Operações Básicas☆-----––•\n")
+
+#SOMA
+resultado = v1 + v2
+print(f"Resultado da soma: {resultado}")
+
+#SUBTRAÇÃO
+resultado = v1 - v2
+print(f"Resultado da subtração: {resultado}")
+
+#MULTIPLICAÇÃO
+resultado = v1 * v2
+print(f"Resultado da multiplicação: {resultado}")
+
+#DIVISÃO
+resultado = v1 / v2
+print(f"Resultado da multiplicação: {resultado:.2f}\n")
+print("•––––-----–----------------☆-------------------------––•\n\n\n")
